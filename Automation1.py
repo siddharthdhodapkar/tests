f9b6456bb7590c855034942a9693f7efaa1e8c05
siddharthdhodapkar <145559359+siddharthdhodapkar@users.noreply.github.com>
2023-09-30 21:18:45 +0530

    Update Automation1.py

--- Automation1.py
+++ Automation1.py
@@ -1,34 +1,39 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Sep 30 20:54:43 2023
-
-@author: lenovo
-"""
-
 import streamlit as st
 import pandas as pd
+import subprocess
 
-# Load data
-uploaded_file = st.file_uploader("Choose an Excel file", type=['xls', 'xlsx'])
+# Check if openpyxl is installed, if not, install it
+try:
+    import openpyxl
+except ImportError:
+    st.write("Installing openpyxl...")
+    subprocess.run(["pip", "install", "openpyxl"])
+    st.write("openpyxl installed successfully!")
 
+# Function to analyze complaints
 def analyze_complaints(data):
     df = pd.read_excel(data)
-    
-    # Table for number of complaints per Zone
     zone_complaints = df.groupby('Zone')['Issue Number'].count().reset_index()
-    st.write("Number of Complaints per Zone")
-    st.table(zone_complaints)
-    
-    # Bar chart for Zone-wise complaints
-    st.bar_chart(zone_complaints.set_index('Zone'))
-    
-    # Table for number of complaints per Subcategory
     subcategory_complaints = df.groupby('Subcategory')['Issue Number'].count().reset_index()
-    st.write("Number of Complaints per Subcategory")
-    st.table(subcategory_complaints)
-    
-    # Bar chart for Subcategory-wise complaints
-    st.bar_chart(subcategory_complaints.set_index('Subcategory'))
+    return zone_complaints, subcategory_complaints
 
+# Streamlit UI
+st.title("Complaints Analysis")
+
+# File upload
+uploaded_file = st.file_uploader("Upload Excel file", type=["xlsx", "xls"])
+
+# If file is uploaded
 if uploaded_file is not None:
-    analyze_complaints(uploaded_file)
+    # Analyze complaints
+    zone_data, subcategory_data = analyze_complaints(uploaded_file)
+
+    # Display zone data
+    st.subheader("Complaints by Zone")
+    st.write(zone_data)
+
+    # Display subcategory data
+    st.subheader("Complaints by Subcategory")
+    st.write(subcategory_data)
+
+    # Add code for bar charts here
